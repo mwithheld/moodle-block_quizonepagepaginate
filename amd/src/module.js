a95544a87615111e73a643233340cbc2b08ab720
mwithheld <796986+mwithheld@users.noreply.github.com>
2022-12-18 10:03:34 -0800

    Scroll to anchor question if present

--- amd/src/module.js
+++ amd/src/module.js
@@ -26,7 +26,7 @@
 class block_quizonepagepaginate {
     constructor(questionsperpage) {
         let debug = false;
-        let self = this;
+        const self = this;
         const FXN = self.constructor.name + '.constructor';
         if (debug) { window.console.log(FXN + '::Started with questionsperpage=', questionsperpage); }
 
@@ -54,18 +54,126 @@ class block_quizonepagepaginate {
 
     run() {
         let debug = false;
-        let self = this;
+        const self = this;
         const FXN = self.constructor.name + '.run';
         if (debug) { window.console.log(FXN + '::Started with self.firstQuestionToShow=; self.questionsperpage=', self.firstQuestionToShow, self.questionsperpage); }
 
         self.getAllQuestions();
-        self.hideShowQuestions(self.firstQuestionToShow, self.questionsperpage);
         self.addNextPrevButtons();
+
+        // Find the question index matching the question-* number.
+        const requestedQuestionIndex = self.getAnchorQuestionIndex();
+        if (debug) { window.console.log(FXN + '::Got requestedQuestionIndex=', requestedQuestionIndex); }
+        if (requestedQuestionIndex >= 0) {
+            self.firstQuestionToShow = requestedQuestionIndex;
+        }
+        self.hideShowQuestions(self.firstQuestionToShow, self.questionsperpage);
+    }
+
+    /**
+     * If the URL anchor value matches /question-\d+-\d+/, get the index of the self.arrQuestions item that matches.
+     *
+     * @returns {number} The matchin index in self.arrQuestions; else -1.
+     */
+    getAnchorQuestionIndex() {
+        let debug = false;
+        const self = this;
+        const FXN = self.constructor.name + '.getAnchorQuestionIndex';
+        if (debug) { window.console.log(FXN + '::Started'); }
+
+        let questionIndex = -1;
+
+        const anchor = self.getAnchor();
+        if (debug) { window.console.log(FXN + '::Got anchor=', anchor); }
+        if (!anchor) {
+            return questionIndex;
+        }
+
+        const questionNrRequested = self.getAnchorQuestionNr(anchor);
+        if (!questionNrRequested) {
+            return questionIndex;
+        }
+
+        questionIndex = self.findQuestionIndexFromQuestionNr(questionNrRequested);
+
+        return questionIndex;
+    }
+
+    /**
+     * Get the URL anchor value.
+     *
+     * @returns {string} The URL anchor value (e.g. "blah" in url=http://bloo#blah); else return empty string.
+     */
+    getAnchor() {
+        return (document.URL.split('#').length > 1) ? document.URL.split('#')[1] : null;
+    }
+
+    /**
+     * Extract the question sequence number from the URL anchor text.
+     *
+     * @param {string} anchor The URL anchor string (e.g. "blah" in url=http://bloo#blah).
+     * @returns {string} The question number e.g. "question-23-9" from the URL anchor value (e.g. from http://bloo#question-23-9); else return empty string.
+     */
+    getAnchorQuestionNr(anchor = '') {
+        let debug = false;
+        const self = this;
+        const FXN = self.constructor.name + '.getAnchorQuestionNr';
+        if (debug) { window.console.log(FXN + '::Started'); }
+
+        // This value is in the format mm-nn where mm=the quiz attempt number; nn=the question index.
+        let questionNrRequested = '';
+
+        if (anchor && anchor.length > 2) {
+            const regexResults = anchor.match(/(question-\d+-\d+)/);
+            if (debug) { window.console.log(FXN + '::Got regexResults=', regexResults); }
+            if (regexResults) {
+                questionNrRequested = regexResults[1];
+            }
+        }
+        if (debug) { window.console.log(FXN + '::Got questionNrRequested=', questionNrRequested); }
+
+        return questionNrRequested;
+    }
+
+    /**
+     * Search self.arrQuestions for a question with number=questionNr.
+     *
+     * @param {str} questionNr The question number e.g. "question-23-9".
+     * @returns {number} The index of self.arrQuestions that matches; else -1.
+     */
+    findQuestionIndexFromQuestionNr(questionNr = '') {
+        let debug = false;
+        const self = this;
+        const FXN = self.constructor.name + '.findQuestionIndexFromQuestionNr';
+        if (debug) { window.console.log(FXN + '::Started'); }
+
+        let indexFound = -1;
+
+        if (!questionNr) {
+            window.console.log(FXN + '::Invalid value passed for param questionNr so return not found');
+            return indexFound;
+        }
+        if (self.arrQuestions.length < 1) {
+            window.console.log(FXN + '::arrQuestions is empty so return not found');
+            return indexFound;
+        }
+
+        self.arrQuestions.forEach(function(elt, index) {
+            if (debug) { window.console.log(FXN + '::Looking at index=; elt=', index, elt); }
+            if (elt.id == questionNr) {
+                if (debug) { window.console.log(FXN + '.forEach::Found matching index=', index); }
+                indexFound = index;
+                return;
+            }
+        });
+
+        if (debug) { window.console.log(FXN + '::About to return indexFound=', indexFound); }
+        return indexFound;
     }
 
     getAllQuestions() {
         let debug = false;
-        let self = this;
+        const self = this;
         const FXN = self.constructor.name + '.getAllQuestions';
         if (debug) { window.console.log(FXN + '::Started'); }
 
@@ -75,7 +183,7 @@ class block_quizonepagepaginate {
 
     hideShowQuestions(first = 0, length) {
         let debug = false;
-        let self = M.block_quizonepagepaginate;
+        const self = M.block_quizonepagepaginate;
         const FXN = self.constructor.name + '.hideShowQuestions';
         if (debug) { window.console.log(FXN + '::Started with start=; length=', first, length); }
 
@@ -95,17 +203,25 @@ class block_quizonepagepaginate {
         });
     }
 
+    scrollToQuestion(anchor = '') {
+        if (anchor.length < 1) {
+            document.querySelector('#responseform').scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    }
+
     setDisplayVal(elt, displayVal) {
         elt.style.display = displayVal;
     }
 
     addNextPrevButtons() {
         let debug = false;
-        let self = this;
+        const self = this;
         const FXN = self.constructor.name + '.addNextPrevButtons';
         if (debug) { window.console.log(FXN + '::Started with self.eltQuizFinishAttemptButtonSelector=', self.eltQuizFinishAttemptButtonSelector); }
 
-        let eltCloneSource = document.querySelector(self.eltQuizFinishAttemptButtonSelector);
+        const eltCloneSource = document.querySelector(self.eltQuizFinishAttemptButtonSelector);
 
         // String are returned in a plain array in the same order specified here.
         // E.g. [0 => "Previous", 1 => "Next"].
@@ -127,24 +243,30 @@ class block_quizonepagepaginate {
                 function(stringsRetrieved) {
                     if (debug) { window.console.log(FXN + '.require.get_strings.then::Started with stringsRetrieved=', stringsRetrieved); }
 
-                    let eltPrevInDom = self.addPrevNextButton(eltCloneSource, 'prev', stringsRetrieved);
+                    const eltPrevInDom = self.addPrevNextButton(eltCloneSource, 'prev', stringsRetrieved);
                     eltPrevInDom.addEventListener('click', self.buttonClickedPrev);
 
-                    let eltNextInDom = self.addPrevNextButton(eltCloneSource, 'next', stringsRetrieved);
+                    const eltNextInDom = self.addPrevNextButton(eltCloneSource, 'next', stringsRetrieved);
                     eltNextInDom.addEventListener('click', self.buttonClickedNext);
                 });
         });
     }
 
     /**
+     * Add buttons to the page to JS-navigate through the quiz questions on the page.
      *
-     * @param {DomElement} eltCloneSource
-     * @param {string} nextorprev
-     * @param {Array<string>} strings
-     * @returns The DomElement we just inserted
+     * @param {DomElement} eltCloneSource An existing button in the form buttons area.
+     * @param {string} nextorprev Which button to create; valid values=[prev, next]
+     * @param {Array<string>} strings Moodle lang strings for the buttons in the order they are created.
+     * @returns {DomElement} The DomElement we just inserted.
      */
     addPrevNextButton(eltCloneSource, nextorprev, strings) {
-        let eltClone = eltCloneSource.cloneNode();
+        let debug = false;
+        const self = M.block_quizonepagepaginate;
+        const FXN = self.constructor.name + '.addPrevNextButton';
+        if (debug) { window.console.log(FXN + '::Started'); }
+
+        const eltClone = eltCloneSource.cloneNode();
         const prevval = (nextorprev == 'prev' ? self.eltBqoppButtonPrev : self.eltBqoppButtonNext);
         const prevdisplay = strings[(nextorprev == 'prev' ? 0 : 1)];
         eltClone.setAttribute('id', prevval);
@@ -159,40 +281,42 @@ class block_quizonepagepaginate {
 
     buttonClickedPrev() {
         let debug = false;
-        let self = M.block_quizonepagepaginate;
+        const self = M.block_quizonepagepaginate;
         const FXN = self.constructor.name + '.buttonClickedPrev';
         if (debug) { window.console.log(FXN + '::Started'); }
 
         self.updateVisibleQuestionRange(false);
         self.hideShowQuestions(self.firstQuestionToShow, self.questionsperpage);
+        self.scrollToQuestion();
     }
 
     buttonClickedNext() {
         let debug = false;
-        let self = M.block_quizonepagepaginate;
+        const self = M.block_quizonepagepaginate;
         const FXN = self.constructor.name + '.buttonClickedNext';
         if (debug) { window.console.log(FXN + '::Started'); }
 
         self.updateVisibleQuestionRange(true);
         self.hideShowQuestions(self.firstQuestionToShow, self.questionsperpage);
+        self.scrollToQuestion();
     }
 
     updateVisibleQuestionRange(getNextSet = true) {
         let debug = false;
-        let self = M.block_quizonepagepaginate;
+        const self = M.block_quizonepagepaginate;
         const FXN = self.constructor.name + '.updateVisibleQuestionRange';
         if (debug) {
             window.console.log(FXN + '::Started with getNextSet=', getNextSet);
         }
 
-        let firstOfAllQs = 0;
-        let lengthToShow = self.questionsperpage;
-        let lastOfAllQs = self.arrQuestions.length;
+        const firstOfAllQs = 0;
+        const lengthToShow = self.questionsperpage;
+        const lastOfAllQs = self.arrQuestions.length;
         if (debug) { window.console.log(FXN + '::Start; firstOfAllQs=' + firstOfAllQs + '; lengthToShow=' + lengthToShow + '; lastOfAllQs=' + lastOfAllQs); }
 
         if (getNextSet) {
             // Propose to jump to the next set of questions.
-            let proposedStart = self.firstQuestionToShow + lengthToShow;
+            const proposedStart = self.firstQuestionToShow + lengthToShow;
             if (debug) { window.console.log(FXN + '::Proposed start of the next set of questions=', proposedStart); }
 
             // Check that the [proposed range of setLength questions] is within the [total range of questions].
@@ -205,7 +329,7 @@ class block_quizonepagepaginate {
             }
         } else {
             // Propose to jump to the next set of questions.
-            let proposedStart = self.firstQuestionToShow - lengthToShow;
+            const proposedStart = self.firstQuestionToShow - lengthToShow;
             window.console.log(FXN + '::Proposed start of the next set of questions=', proposedStart);
 
             // Check that the [proposed range of setLength questions] is within the [total range of questions].
@@ -237,6 +361,6 @@ export const init = (questionsperpage) => {
         //if (debug) { window.console.log('M.block_quizonepagepaginate::Built class=', M.block_quizonepagepaginate); }
         M.block_quizonepagepaginate.run();
     } catch (e) {
-        window.console.log.error(e);
+        window.console.error(e);
     }
 };
