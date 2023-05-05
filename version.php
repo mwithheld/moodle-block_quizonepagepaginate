98d8329cfd7e5df915365816ed5b0777fb8c929e
mwithheld <796986+mwithheld@users.noreply.github.com>
2023-05-05 07:31:41 -0700

    Add release and maturity

--- version.php
+++ version.php
@@ -32,4 +32,6 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'block_quizonepagepaginate';
 $plugin->version = 2023050300;
+$plugin->release = 2023050300;
 $plugin->requires = 2018051700; // Moodle 3.5.0.
+$plugin->maturity = MATURITY_BETA;
